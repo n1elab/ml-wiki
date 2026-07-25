a5472b9ce18b6a78f74f8a69a26fe868d8cdc673
neniya222 <234392619+neniya222@users.noreply.github.com>
2026-07-25 13:23:14 +0900

    Add: Youtube & X Links in social

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,8 @@ export default defineConfig({
 			title: 'MiLight Server Wiki💡ミライト鯖 Wiki',
 			social: [
 				{ icon: 'discord', label: 'Discord', href: 'http://discord.gg/UyKV4BDxBR' },
+				{ icon: 'x.com', label: 'X', href: 'https://x.com/ml_ms2' },
+				{ icon: 'youtube', label: 'youtube', href: 'https://www.youtube.com/channel/UC_p9eD-ZmbDRSJ5HsvDqcoQ?sub_confirmation=1' },
 				{ icon: 'pencil', label: 'Edit', href: 'https://github.com/n1elab/ml-wiki/' }
 			],
 			sidebar: [
@@ -24,6 +26,15 @@ export default defineConfig({
 				{
 					label: 'Reference',
 					items: [{ autogenerate: { directory: 'reference' } }],
+				},
+				{
+					label: '⛓️ 関連リンク',
+					items: [
+						{ label: '🏠 公式サイト', link: 'http://mlms.n1e.net' },
+						{ label: '📄 公式Wiki', link: 'http://wiki-mlms.n1e.net' },
+						{ label: '🗺️ ウェブマップ', link: 'http://map-mlms.n1e.net' },
+						{ label: '💬 公式Discord', link: 'http://discord.gg/UyKV4BDxBR' },
+					],
 				},
 			],
 		}),
